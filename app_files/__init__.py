46a4ad399c51059f06ec3251e0e82520353f5063
ldriveron <ldriveron@gmail.com>
2020-02-05 12:31:54 -0500

    HTTPS Redirect

--- app_files/__init__.py
+++ app_files/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_talisman import Talisman
 
 app = Flask(__name__)
 app._static_folder = "static"  # set a static files folder for Flask app
+Talisman(app)
 
 from app_files import routes  # import the routes python file
